b0917df256e16826062bc85f10ddee8cd6b500cb
yashvisuals <yashdbs001@gmail.com>
2023-08-13 01:22:18 +0530

    My Eatery webpage with chatbot

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript to handle opening and closing chatbot window
+document.getElementById('openChatbot').addEventListener('click', function() {
+    var chatbotWindow = document.getElementById('chatbotWindow');
+    chatbotWindow.classList.toggle('open');
+});
