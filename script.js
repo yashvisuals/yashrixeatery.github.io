e21e7fe8ce4ee48ef019c4a546490510824221e3
yashvisuals <yashdbs001@gmail.com>
2024-05-24 00:03:40 +0530

    update

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ document.getElementById('openChatbot').addEventListener('click', function() {
     var chatbotWindow = document.getElementById('chatbotWindow');
     chatbotWindow.classList.toggle('open');
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var toggle = document.getElementById('nav-toggle');
+    var menu = document.getElementById('nav-menu');
+
+    toggle.addEventListener('click', function() {
+        menu.classList.toggle('active');
+    });
+});
