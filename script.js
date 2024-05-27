b2dad208cb0ebab2329c820a0bc368881ccd02c2
yashvisuals <yashdbs001@gmail.com>
2024-05-27 11:24:37 +0530

    update

--- script.js
+++ script.js
@@ -7,8 +7,27 @@ document.getElementById('openChatbot').addEventListener('click', function() {
 document.addEventListener('DOMContentLoaded', function() {
     var toggle = document.getElementById('nav-toggle');
     var menu = document.getElementById('nav-menu');
+    const hamburger = document.querySelector('.fas fa-bars');
+    const cross = document.querySelector('.fas fa-times');
+    const navbar = document.querySelector('.navbar');
+    const links = document.querySelectorAll('.nav-menu a'); // Select all navbar links
 
     toggle.addEventListener('click', function() {
         menu.classList.toggle('active');
     });
+
+    hamburger.addEventListener('click', () => {
+        navbar.classList.add('nav-active');
+    });
+
+    cross.addEventListener('click', () => {
+        navbar.classList.remove('nav-active');
+    });
+
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            navbar.classList.remove('nav-active');
+        });
+    });
+
 });
